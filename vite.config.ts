c3b54909dddd05e90d25fd69b6bf6b02b3ff36f3
seana02 <seanseptember2002@gmail.com>
2026-05-15 23:27:04 -0400

    pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+    plugins: [devtools(), solidPlugin()],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: 'esnext',
+    },
+    base: 'https://seana02.github.io/contest-grader/',
 });
